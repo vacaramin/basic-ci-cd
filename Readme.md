9bfe6e6a2ee6a697fc0a2d7bc68a73714cd8de55
Waqar Amin <vacaramin86@gmail.com>
2024-03-30 17:05:57 +0500

    skeleton code for repo

---
+++ Readme.md
@@ -0,0 +1,35 @@
+# Basic CI-CD Test
+
+setting up a basic CI/CD pipeline for a web application. The application consists of a simple static website and a basic RESTful API.
+
+## Tasks
+
+1. Version Control (Git):
+
+    - [x] Create a Git repository for any test web application.
+    - [x] Commit the initial codebase including the static website and any sample test API.
+
+2. CI/CD Pipeline
+
+    - [x] Choose a CI/CD tool (e.g., **GitHub Actions**, Jenkins).
+    - [x] Set up a basic pipeline that triggers on each commit to the develop branch.
+    - [ ] Include stages for building and deploying both the static website and API.
+
+3. Automated Testing:
+
+    - [ ] Implement a simple automated test for the API (e.g., a basic endpoint response check).
+    - [ ] Ensure that the CI/CD pipeline fails if the test fails.
+
+4. Deployment:
+
+    - [ ] Deploy the static website to a simple web server.
+    - [ ] Deploy the API to a server, ensuring that it can handle basic HTTP requests.
+
+### Project Structure
+
+[Architecture diagram - Draw.io](https://drive.google.com/file/d/1qU1IhZAexsxA4jlTcJkS5mX_Tzvqf1ez/view?usp=sharing "Architecture diagram")
+
+### Deployments
+
+- [Frontend](https://ci.vacaramin.me "frontend")
+- [Backend](https://vacaramin.me "backend")
---
+++ backend/main.go
@@ -0,0 +1,238 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Users struct {
+	Name string
+	Age  int
+}
+
+func main() {
+	loadENV()
+
+	db := initDB()
+	defer db.Close()
+
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/users", getUsersHandler(db))
+	http.HandleFunc("/user", createUserHandler(db))
+	http.HandleFunc("/update", updateUserHandler(db))
+	http.HandleFunc("/delete", deleteUserHandler(db))
+	http.HandleFunc("/404", NotFoundHandler)
+	http.HandleFunc("/ping", pingHandler)
+	http.HandleFunc("/pong", pongHandler)
+
+	http.ListenAndServe(os.Getenv("PORT"), nil)
+}
+
+func loadENV() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
+func initDB() *sql.DB {
+	db, err := sql.Open("sqlite3", "./users.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	createTableSQL := `
+	CREATE TABLE IF NOT EXISTS users (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		name TEXT,
+		age INTEGER
+	);`
+
+	_, err = db.Exec(createTableSQL)
+	if err != nil {
+		log.Fatal("Error creating users table: ", err)
+	}
+
+	// Insert sample data
+	insertDataSQL := `
+	INSERT INTO users (name, age) VALUES
+	('John Doe', 30),
+	('Alice Smith', 25),
+	('Bob Johnson', 40);
+	`
+
+	_, err = db.Exec(insertDataSQL)
+	if err != nil {
+		log.Fatal("Error inserting sample data: ", err)
+	}
+
+	return db
+}
+
+// Handlers for CRUD operations
+func getUsersHandler(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		users := []User{}
+		rows, err := db.Query("SELECT id, name, age FROM users")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		defer rows.Close()
+
+		for rows.Next() {
+			var user User
+			err := rows.Scan(&user.ID, &user.Name, &user.Age)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			users = append(users, user)
+		}
+
+		jsonResponse, err := json.Marshal(users)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write(jsonResponse)
+	}
+}
+
+func createUserHandler(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var user User
+		err := json.NewDecoder(r.Body).Decode(&user)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		result, err := db.Exec("INSERT INTO users (name, age) VALUES (?, ?)", user.Name, user.Age)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		userID, err := result.LastInsertId()
+		user.ID = int(userID)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		jsonResponse, err := json.Marshal(user)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusCreated)
+		w.Write(jsonResponse)
+	}
+}
+func updateUserHandler(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var user User
+		err := json.NewDecoder(r.Body).Decode(&user)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		_, err = db.Exec("UPDATE users SET name = ?, age = ? WHERE id = ?", user.Name, user.Age, user.ID)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+	}
+}
+
+func deleteUserHandler(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var user User
+		err := json.NewDecoder(r.Body).Decode(&user)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		_, err = db.Exec("DELETE FROM users WHERE id = ?", user.ID)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+	}
+}
+
+// User struct
+type User struct {
+	ID   int    `json:"id,omitempty"`
+	Name string `json:"name,omitempty"`
+	Age  int    `json:"age,omitempty"`
+}
+
+func homeHandler(w http.ResponseWriter, _ *http.Request) {
+	response := map[string]string{"hello": "world"}
+
+	jsonResponse, err := json.Marshal(response)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(jsonResponse)
+
+}
+func pingHandler(w http.ResponseWriter, _ *http.Request) {
+	response := map[string]string{
+		"Message": "Pong",
+	}
+	jsonResponse, err := json.MarshalIndent(response, "", "2")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(jsonResponse)
+
+}
+
+func pongHandler(w http.ResponseWriter, _ *http.Request) {
+	response := map[string]string{
+		"Message": "Ping",
+	}
+	jsonResponse, err := json.MarshalIndent(response, "", "2")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(jsonResponse)
+
+}
+
+func NotFoundHandler(w http.ResponseWriter, r *http.Request) {
+	response := map[string]string{"error": "404- Not found"}
+	jsonResponse, err := json.Marshal(response)
+	if err != nil {
+		log.Println("failed to marshal json")
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusNotFound)
+	w.Write(jsonResponse)
+}
